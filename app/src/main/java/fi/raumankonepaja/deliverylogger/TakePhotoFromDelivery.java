29252bd5e221cc19f081ad12c55bc5a823afb2e3
Stenu <sami.stenvall@student.tut.fi>
2017-10-10 18:18:12 +0300

    Updated project. Fixed some issues.

--- app/src/main/java/fi/raumankonepaja/deliverylogger/TakePhotoFromDelivery.java
+++ app/src/main/java/fi/raumankonepaja/deliverylogger/TakePhotoFromDelivery.java
@@ -1,7 +1,9 @@
 package fi.raumankonepaja.deliverylogger;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -15,33 +17,55 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 
+
 public class TakePhotoFromDelivery extends AppCompatActivity {
 
-    Uri photoURI;
+    private static final String TAG = "TakePhotoFromDelivery";
+    private static final String KEY_M_CURRENT_PHOTO_PATH = "mCurrentPhotoPath";
+    private static final String KEY_M_LAST_CURRENT_PHOTO_PATH = "mLastCurrentPhotoPath";
+    private boolean pictureSent;
 
+    Uri photoURI;
     String mCurrentPhotoPath;
+    String mLastCurrentPhotoPath;
+
+    EditText deliveryNumber;
+    EditText deliveryPosition;
+    Button shootButton;
+    ImageView takenPictureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo_from_delivery);
 
-        EditText deliveryNumber = (EditText) findViewById(R.id.editTextDeliveryNumber);
-        EditText deliveryPosition = (EditText) findViewById(R.id.editTextDeliveryPosition);
+        deliveryNumber = (EditText) findViewById(R.id.editTextDeliveryNumber);
+        deliveryPosition = (EditText) findViewById(R.id.editTextDeliveryPosition);
+        shootButton = (Button) findViewById(R.id.shootButton);
+        takenPictureImageView = (ImageView) findViewById(R.id.takenPictureImageView);
+
+        pictureSent = false;
+
 
         TextChangeListener textChangeListener = new TextChangeListener();
 
-        ImageView takenPictureImageView = (ImageView) findViewById(R.id.takenPictureImageView);
 
         // piilotetaan heti ekaksi  nappula
         showHideButton();
 
         deliveryNumber.addTextChangedListener(textChangeListener);
         deliveryPosition.addTextChangedListener(textChangeListener);
+
+        // ei hukkaa kuvaa orientaatiota vaihdettaessa
+        if (savedInstanceState != null) {
+            mCurrentPhotoPath = savedInstanceState.getString(KEY_M_CURRENT_PHOTO_PATH);
+            mLastCurrentPhotoPath = savedInstanceState.getString(KEY_M_LAST_CURRENT_PHOTO_PATH);
+        }
 
 
     }
@@ -50,18 +74,77 @@ public class TakePhotoFromDelivery extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // täällä tarvii ratkaista pari asiaa
-        // TODO otettu kuva pitää kääntää oikeaan asentoon
-        // TODO kuva katoaa puhelinta käännettäessä, korjaa tämä
+        // tarvittaessa upataan tiedot firebaseen
+        if(mCurrentPhotoPath != null){
+            File photoFile = new File (mCurrentPhotoPath);
+            Log.i(TAG, "onResume metodissa katsomassa onko kuva vaihtunut");
+            Log.i(TAG, "tiedoston koko:" + Long.toString(photoFile.length()));
+
+            if(photoFile.length() != 0 && mCurrentPhotoPath != mLastCurrentPhotoPath ){
+                mLastCurrentPhotoPath = mCurrentPhotoPath;
+
+                uploadDataToFirebase();
+            }
+
+            // jos on palattu kuvaus intentistä ilman kuvaa niin ei hukata viimeistä otettua kuvaa
+            if(photoFile.length() == 0){
+                mCurrentPhotoPath = mLastCurrentPhotoPath;
+            }
+
+        }
+
+
+
 
         // vaihdetaan kuva samalla imgavieviin
-        Log.d("hoi", "täällä ollaan");
+        Log.d(TAG, " onResume() - metodissa");
 
         if (mCurrentPhotoPath != null) {
-            ImageView takenPictureImageView = (ImageView) findViewById(R.id.takenPictureImageView);
+
             takenPictureImageView.setVisibility(View.VISIBLE);
-            takenPictureImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+
+            Bitmap takenPictureImage = (BitmapFactory.decodeFile(mCurrentPhotoPath));
+            takenPictureImageView.setImageBitmap(takenPictureImage);
+
+            // katsotaan missä asennossa kuva on otettu ja käännetään
+            // imageview oikeaan asentoon
+
+            try {
+                Log.i(TAG, " ExifInterface kohdassa");
+
+                ExifInterface exif = new ExifInterface(mCurrentPhotoPath.toString());
+
+                int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                int rotation = 0;
+
+                if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                    rotation = 90;
+                } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                    rotation = 180;
+                } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                    rotation = 270;
+                }
+
+                takenPictureImageView.setRotation(rotation);
+
+            } catch (Exception e) {
+                Log.e(TAG, "exifInterface lataus ei onnistunut");
+                Log.e(TAG, e.toString());
+                Log.e(TAG, "mCurrentPhotoPath :" + mCurrentPhotoPath.toString());
+            }
+
+
         }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(TAG, "onSaveInstanceState - metodissa");
+
+        outState.putString(KEY_M_CURRENT_PHOTO_PATH, mCurrentPhotoPath);
 
     }
 
@@ -87,13 +170,10 @@ public class TakePhotoFromDelivery extends AppCompatActivity {
     // metodi joka piilottaa kuvaa napin jos tietoja ei ole tarpeeksi
     private void showHideButton() {
 
-        Button shootButton = (Button) findViewById(R.id.shootButton);
-        EditText deliveryNumber = (EditText) findViewById(R.id.editTextDeliveryNumber);
-        EditText deliveryPosition = (EditText) findViewById(R.id.editTextDeliveryPosition);
 
         if (deliveryNumber.getText().length() > 0 && deliveryPosition.getText().length() > 0) {
             shootButton.setVisibility(View.VISIBLE);
-            Log.i("showHideButton metodi:", " näytä nappi");
+            Log.i(TAG, "showHideButton metodi: näytä nappi");
         } else {
             shootButton.setVisibility(View.INVISIBLE);
         }
@@ -115,7 +195,7 @@ public class TakePhotoFromDelivery extends AppCompatActivity {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
-                Log.e("ennen kuvan ottoa ", "ongelmia " + ex.getMessage().toString());
+                Log.e(TAG, "ongelmia " + ex.getMessage().toString());
 
             }
             // Continue only if the File was successfully created
@@ -133,8 +213,7 @@ public class TakePhotoFromDelivery extends AppCompatActivity {
     }
 
 
-
-// tässä luodaan uusi tiedosto, johon kuva sitten tallennetaan...
+    // tässä luodaan uusi tiedosto, johon kuva sitten tallennetaan...
     private File createImageFile() throws IOException {
         // Create an image file name
         // String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -152,8 +231,12 @@ public class TakePhotoFromDelivery extends AppCompatActivity {
         return image;
     }
 
+    private void uploadDataToFirebase (){
 
+        Toast toast = Toast.makeText(this," FIREBASE lähetys", Toast.LENGTH_SHORT);
+        toast.show();
 
+    }
 
 
 }
